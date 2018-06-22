3194735e9c5c04b6e58491c7601414a6b14a01e5
Rafayel Gardishyan <rgardishyan@gmail.com>
2018-06-22 16:12:24 +0200

    Update settings.py

--- Sorter/settings.py
+++ Sorter/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uudns!pzn-h@d5rjbhlx00(%dau48em1ji7+#6#i%1(*868k_n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
