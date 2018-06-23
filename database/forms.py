0792e70709526fe3319c8b24ac6470876d763500
Rafayel Gardishyan <rgardishyan@gmail.com>
2018-06-23 14:08:39 +0200

    Update forms.py

--- database/forms.py
+++ database/forms.py
@@ -4,7 +4,7 @@ from .constants import SHAPES, TEXTURES, COLORS
 
 
 class FruitForm(forms.Form):
-    size = forms.IntegerField(label="Size (cm)")
+    size = forms.IntegerField(label="Size (cm)", max_value=100)
     color = forms.ChoiceField(choices=COLORS, widget=forms.Select)
     texture = forms.ChoiceField(choices=TEXTURES, widget=forms.Select)
     shape = forms.ChoiceField(choices=SHAPES, widget=forms.Select)
